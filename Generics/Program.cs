4f3dc78c067b2ec3dbc3249bde643e9bf4648ebb
tnhnyldrm <46500254+tnhnyldrm@users.noreply.github.com>
2023-12-02 12:07:49 +0300

    generics

---
+++ Generics/Program.cs
@@ -0,0 +1,11 @@
+﻿
+using Generics;
+
+List<string> sehirler = new List<string>();
+
+MyList<string> sehirler2 = new MyList<string>();
+sehirler2.Add("ankara");
+sehirler2.Add("ankara");
+sehirler2.Add("ankara");
+sehirler2.Add("ankara");
+Console.WriteLine(sehirler2.Count);
