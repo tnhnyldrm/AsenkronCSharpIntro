f4c13c7868109140d91cc3156399672c0dfec13b
tnhnyldrm <46500254+tnhnyldrm@users.noreply.github.com>
2023-12-02 11:24:13 +0300

    Constructors

---
+++ Constructors/Customer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors
+{
+    public class Customer
+    {
+        //default constructor
+        public Customer()
+        {
+
+        }
+        public Customer(int id, string firstName, string lastName, string city)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            City = city;
+        }
+
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+    }
+}
---
+++ Constructors/Program.cs
@@ -0,0 +1,6 @@
+﻿using Constructors;
+
+Customer customer1 = new Customer { Id=1, FirstName="engin", LastName="demiroğ", City="ankara"};
+Customer customer2 = new Customer ( 2, "derin", "demiroğ", "ankara" );
+
+Console.WriteLine(customer2.FirstName);
