619c5aaac1103d79c86dbfea2c641ca403720530
tnhnyldrm <46500254+tnhnyldrm@users.noreply.github.com>
2023-12-02 11:11:02 +0300

    Add project files.

---
+++ AsenkronCSharpIntro/PersonManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsenkronCSharpIntro
+{
+    public class PersonManager
+    {
+        public void Add(Person person)
+        {
+            Console.WriteLine(person.FirstName);
+        }
+    }
+}
---
+++ AsenkronCSharpIntro/Program.cs
@@ -0,0 +1,40 @@
+﻿using AsenkronCSharpIntro;
+
+//int sayi1 = 10;
+//int sayi2 = 20;
+
+//sayi1 = sayi2;
+//sayi2 = 100;
+
+//Console.WriteLine("sayi 1: "+sayi1);
+
+//int[] sayilar1 = new int[] {1,2,3};
+//int[] sayilar2 = new int[] { 10, 20, 30 };
+
+//sayilar1 = sayilar2;
+//sayilar2[0] = 1000;
+
+//Console.WriteLine("Sayılar[0]= " + sayilar1[0]);
+
+Person person1 = new Person();
+Person person2 = new Person();
+person1.FirstName = "Engin";
+
+person2 = person1;
+person1.FirstName = "Derin";
+//Console.WriteLine(person2.FirstName);
+
+Customer customer = new Customer();
+customer.FirstName = "Salih";
+customer.CreditCardNumber = "123456789";
+
+Employee employee = new Employee();
+
+Person person3 = customer;
+customer.FirstName = "Ahmet";
+
+//Console.WriteLine(((Customer)person3).CreditCardNumber);
+
+PersonManager personManager = new PersonManager();
+personManager.Add(customer);
+
